9b0ba6757d7a0d438c8134527f3365153c42b6e2
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-06-27 17:50:17 +0100

    tidy

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,3 @@
 export * from "../gen/ts/surrealdb/protocol/rpc/v1/rpc";
-export * from "../gen/ts/surrealdb/protocol/v1/document";
 export * from "../gen/ts/surrealdb/protocol/v1/expr";
 export * from "../gen/ts/surrealdb/protocol/v1/value";
